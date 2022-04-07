46fc46f5523867fb3a5bd4a58ebdcd079eadf5b5
Doreille sabrina <s.doreille@etudiant.ecolemelies.fr>
2022-04-07 15:26:17 +0200

    Création button pour sceneTest
    Dog, Snake, Giant, Draugr

---
+++ Assets/Script/UI/Draugr_Scene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Draugr_Scene : MonoBehaviour
+{
+    public void OnDraugr()
+    {
+        SceneManager.LoadScene("Draugr");
+    }
+
+}
