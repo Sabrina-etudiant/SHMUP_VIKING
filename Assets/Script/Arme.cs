3d31fcbe69c28002e303270b653d749db3483a1a
Doreille sabrina <s.doreille@etudiant.ecolemelies.fr>
2022-06-09 18:58:03 +0200

    changement et ajout ligne et script

--- Assets/Script/Arme.cs
+++ Assets/Script/Arme.cs
@@ -1,5 +1,56 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Collections;
+public class Arme : MonoBehaviour
+{
+    public Transform[] point;
+    public GameObject BulletPrefab;
+    public float sceptreCooldown;
+    public float spellCooldown;
+    public bool canShoot;
+    public bool canSpell;
+    private void Start()
+    {
+        canShoot = true;
+        canSpell = true;
+    }
+    void Update()
+    {
+    }
+    public void OnShoot(InputValue value)
+    {
+        if (value.isPressed && canShoot == true)
+        {
+            Instantiate(BulletPrefab, point[0].position, point[0].rotation);
+            Instantiate(BulletPrefab, point[1].position, point[1].rotation);
+            StartCoroutine(sceptreFireRate());
+        }
+    }
+    public void OnSpell(InputValue value)
+    {
+        if (value.isPressed && canSpell == true)
+        {
+            Instantiate(BulletPrefab, point[2].position, point[2].rotation);
+            StartCoroutine(spellFireRate());
+        }
+    }
+    IEnumerator sceptreFireRate()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(sceptreCooldown);
+        canShoot = true;
+    }
+    IEnumerator spellFireRate()
+    {
+        canSpell = false;
+        yield return new WaitForSeconds(spellCooldown);
+        canSpell = true;
+    }
+}
+
+
+/*using UnityEngine.InputSystem;
+using UnityEngine;
 
 public class Arme : MonoBehaviour
 {
@@ -18,5 +69,5 @@ public class Arme : MonoBehaviour
             Instantiate(BulletPrefab, point.position, point.rotation);
         }
     }
-}
+}*/
 
